3ee229b3934bb30e7f8cf7a45d747369fb3fbcc3
Cong <xiaohanzi1001@gmail.com>
2017-04-14 22:25:46 +0200

    implement part4

--- app/controllers/jobs_controller.rb
+++ app/controllers/jobs_controller.rb
@@ -3,9 +3,11 @@ class JobsController < ApplicationController
   def show
     @job = Job.find(params[:id])
   end
+
   def index
-    @jobs = Job.all
+    @jobs = Job.where(:is_hidden => false).order("created_at DESC")
   end
+
 def new
   @job = Job.new
 end
@@ -42,7 +44,7 @@ end
 private
 
 def job_params
-  params.require(:job).permit(:title, :description, :wage_upper_bound, :wage_lower_bound, :contact_email)
+  params.require(:job).permit(:title, :description, :wage_upper_bound, :wage_lower_bound, :contact_email, :is_hidden)
 end
 
 
